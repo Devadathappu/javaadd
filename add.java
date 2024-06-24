11bb7be3b0add9771333b9faa1edbcdeda3eaf26
Devadathappu <142972061+Devadathappu@users.noreply.github.com>
2024-06-24 10:11:20 +0530

    Create add.java

---
+++ add.java
@@ -0,0 +1,17 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Define two numbers
+        int number1 = 10;
+        int number2 = 20;
+
+        // Add the two numbers
+        int sum = number1 + number2;
+
+        // Print the numbers
+        System.out.println("The first number is: " + number1);
+        System.out.println("The second number is: " + number2);
+
+        // Print the sum of the numbers
+        System.out.println("The sum of the two numbers is: " + sum);
+    }
+}
